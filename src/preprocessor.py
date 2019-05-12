70cbe244f77bcd17149c66393b3c3ab728562e2d
Joseph Seiler <jseiler97@gmail.com>
2019-05-11 21:05:15 -0400

    added preprocessing

---
+++ src/preprocessor.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import sklearn 
+import numpy as np 
+import re
+from nltk.stem import *
+import threading
+
+def main():
+	train = pd.read_csv("train.csv")
+	test = pd.read_csv("test.csv")
+	clean(train)
+	clean(test)
+	train.to_csv("clean_train.csv", index=False, encoding='utf8')
+	test.to_csv("clean_test.csv", index=False, encoding='utf8')
+
+def clean(data):
+	clean_tweets = []
+	for tweet in data.tweet:
+		noiseless = remove_noise(tweet)
+		stemmed = to_stems(noiseless,False)
+		clean_tweets.append(stemmed)
+	data.tweet = clean_tweets
+
+#preprocessing steps:
+#all lower case -> already done
+#stemming -> Snowball
+#stopword removal -> in stemmer
+#normalization (ex: gud -> good / goooood -> good) -> we'll see if we need
+#noise removal (remove symbols and numbers) -> use regex include hashtags and apostrophes
+
+def remove_noise(words:str) -> str:
+	regex = re.compile('[^a-zA-Z#\']')
+	return regex.sub(' ',words)
+
+def to_stems(words:str, stopword:bool) -> list:
+	stemmer = SnowballStemmer('english',ignore_stopwords = stopword)
+	stems = []
+	for word in words.split():
+		stemmer.stem(word)
+		stems.append(word)
+	return stems
+
+if __name__ == '__main__':
+	main()
